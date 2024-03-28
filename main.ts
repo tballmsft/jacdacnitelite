73c7bb94e6263888a82c316dc2264f6f1dc20154
Tom Ball <tballmsft@users.noreply.github.com>
2024-03-28 15:10:05 -0700

    refactor

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
+
 let niteLite = false
+basic.showIcon(IconNames.SmallSquare)
 
-modules.accelerometer1.onEvent(jacdac.AccelerometerEvent.FaceUp, function() {
-    basic.showIcon(IconNames.No)
-    niteLite = false
-    modules.led1.setAll(0x000000)
-})
-
-modules.accelerometer1.onEvent(jacdac.AccelerometerEvent.FaceDown, function () {
-    basic.showIcon(IconNames.Yes)
-    niteLite = true
-})
-
-modules.lightLevel1.onLightLevelChangedBy(5, function() {
+function updateLed() {
     if (niteLite) {
         if (modules.lightLevel1.lightLevel() < 20)
             modules.led1.setAll(0x00ff00)
         else
             modules.led1.setAll(0x000000)
     }
+}
+modules.accelerometer1.onEvent(jacdac.AccelerometerEvent.FaceUp, function() {
+    basic.showIcon(IconNames.No)
+    niteLite = false
+    modules.led1.setAll(0x000000)
 })
-
+modules.accelerometer1.onEvent(jacdac.AccelerometerEvent.FaceDown, function () {
+    basic.showIcon(IconNames.Yes)
+    niteLite = true
+    updateLed()
+})
+modules.lightLevel1.onLightLevelChangedBy(5, function() {
+    updateLed()
+})
 modules.potentiometer1.onPositionChangedBy(5, function() {
     modules.led1.setBrightness(modules.potentiometer1.position())
 })
